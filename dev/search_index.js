e86d86b99769fe19db4a52f2539f5ec25d071e40
Documenter.jl <documenter@juliadocs.github.io>
2023-11-30 08:20:25 +0000

    build based on 8670325

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SliceSampling","category":"page"},{"location":"#SliceSampling","page":"Home","title":"SliceSampling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SliceSampling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SliceSampling]","category":"page"}]
+}
