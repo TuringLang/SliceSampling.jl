823e8d056cace3207653eff62ab858c5ddce04d8
Ray Kim <msca8h@naver.com>
2024-08-12 01:03:39 +0100

    fix misleading doc

--- src/multivariate/randpermgibbs.jl
+++ src/multivariate/randpermgibbs.jl
@@ -9,7 +9,7 @@ This applies `unislice` coordinate-wise in a random order.
 - `unislice::Union{<:AbstractUnivariateSliceSampling,<:AbstractVector{<:AbstractUnivariateSliceSampling}}`: a single or a vector of univariate slice sampling algorithms.
 
 When `unislice` is a vector of samplers, each slice sampler is applied to the corresponding coordinate of the target posterior.
-Furthermore, the `length(unislice)` must match the dimensionality of the posterior.
+In that case, the `length(unislice)` must match the dimensionality of the posterior.
 """
 struct RandPermGibbs{
     S <: Union{
