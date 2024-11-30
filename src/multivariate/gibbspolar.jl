e738b1d6382a85395b0e056912c87779022a2757
Kyurae Kim <kyrkim@seas.upenn.edu>
2024-11-30 15:59:11 -0500

    fix docs move infos and warnings for `GibbsPolarSlice` to docstring

--- src/multivariate/gibbspolar.jl
+++ src/multivariate/gibbspolar.jl
@@ -10,6 +10,15 @@ Gibbsian polar slice sampling algorithm by P. Schär, M. Habeck, and D. Rudolf [
 # Keyword Arguments
 - `w::Real`: Initial window size for the radius shrinkage procedure
 - `max_proposals::Int`: Maximum number of proposals allowed until throwing an error (default: `10^6`).
+
+!!! info
+    By the nature of polar coordinates, GPSS only works reliably for targets with dimension at least \$\$d \\geq 2\$\$.
+
+!!! warning
+    When initializing the chain (*e.g.* the `initial_params` keyword arguments in `AbstractMCMC.sample`), it is necessary to inialize from a point \$\$x_0\$\$ that has a sensible norm \$\$\\lVert x_0 \\rVert > 0\$\$, otherwise, the chain will start from a pathologic point in polar coordinates. This might even result in the sampler getting stuck in an infinite loop. (This can be prevented by setting `max_proposals`.) If \$\$\\lVert x_0 \\rVert \\leq 10^{-5}\$\$, the current implementation will display a warning. 
+	
+!!! info
+    For Turing users: `Turing` might change `initial_params` to match the support of the posterior. This might lead to \$\$\\lVert x_0 \\rVert\$\$ being small, even though the vector you passed to`initial_params` has a sufficiently large norm. If this is suspected, simply try a different initialization value.
 """
 struct GibbsPolarSlice{W <: Real} <: AbstractMultivariateSliceSampling
     w::W
